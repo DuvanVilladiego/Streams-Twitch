5189e45004697133c707ffd6bd0b8afe41e91e28
Duvan <duvan.villadiego.oficial@gmail.com>
2021-05-17 22:00:17 -0500

    repo update

--- JavaScript/Dia #1/scripts/js/operacionesConVariables.js
+++ JavaScript/Dia #1/scripts/js/operacionesConVariables.js
@@ -1,17 +1,24 @@
-// Operaciones con numeros enteros y decimales
-function llamar() {
-  let num1 = parse(prompt("Digite un numero para sumar: "));
-  let num2 = parse(prompt("Digite otro numero para sumar: "));
-  alert(num1 + " + " + num2 + " es igual a = " + (num1 + num2));
-}
-
-function parse(numero) {
-    return parseFloat(parseFloat(numero).toFixed(2));
-}
-
 // eventos con botones
 const boton = document.getElementById("boton");
 
 boton.addEventListener("click", () => {
-    llamar()
+    operacion()
 });
+
+// Operaciones con numeros enteros y decimales
+operacion = () => {
+    let num1 = parse(prompt("Digite un numero para sumar: "));
+    let num2 = parse(prompt("Digite otro numero para sumar: "));
+  
+    incluir(num1 + " + " + num2 + " es igual a = " + (num1 + num2))
+  }
+  function parse(numero) {
+    return parseFloat(parseFloat(numero).toFixed(2));
+  }
+
+// incluir operación en el DOM
+const test = document.getElementById("test")
+
+incluir = (pintar) => {
+    test.innerHTML += "</br>" + pintar;
+}
