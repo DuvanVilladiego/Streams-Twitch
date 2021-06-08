b19202b519d019319960c5e75359b5a1b5e8b61a
Duvan <duvan.villadiego.oficial@gmail.com>
2021-06-07 22:08:47 -0500

    Proyecto hamburguesa avances dia #2

--- JavaScript/Hamburguesa/assets/js/imageConverter.js
+++ JavaScript/Hamburguesa/assets/js/imageConverter.js
@@ -1,16 +1,42 @@
-const FileSaver = require('file-saver')
-let boton = document.getElementById("boton")
+// CONTENEDOR DE IMAGENES
+const imageContent = document.getElementById("imageContent")
+const ingredients = document.getElementById("ingredients")
 
-function ImageCreate(){
-	html2canvas([document.getElementById('imageContent')], {
-		onrendered: function (canvas) {
-			var img = canvas.toDataURL('image/png'); //o por 'image/jpeg' 
-			//display 64bit imag
-            FileSaver.saveAs(img, "Hamburguesa personal.png")
-        }
-	})
+let boton = document.getElementById("boton");
+
+// BOTONES DE INGREDIENTES
+let lechuga = document.getElementById("lechuga")
+let carne = document.getElementById("carne")
+let cebolla = document.getElementById("cebolla")
+let pepinillos = document.getElementById("pepinillos")
+let queso = document.getElementById("queso")
+let tomate = document.getElementById("tomate")
+
+function ImageCreate() {
+  html2canvas([imageContent], {
+    onrendered: function (canvas) {
+      let img = canvas.toDataURL("image/png"); //o por 'image/jpeg'
+      //display 64bit imag
+      let a = document.createElement("a");
+      a.download = true;
+      a.target = "_blank";
+      a.href = img;
+      a.click();
+    },
+  });
 }
 
-boton.addEventListener('click', () =>{
-    ImageCreate()
+function addIngredient(ingrediente) {
+	ingredients.innerHTML += `
+	<img src="./assets/img/png/${ingrediente}.png" alt="">
+	`
+}
+
+
+boton.addEventListener("click", () => {
+  ImageCreate();
+});
+
+lechuga.addEventListener("click", () => {
+	addIngredient('lechuga')
 })
